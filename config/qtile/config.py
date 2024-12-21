f055dc99b12c062f53275db539ff66f43794f9b9
Nicolás Cerpa <nicolas.cerpa@sansano.usm.cl>
2024-12-21 12:25:29 -0300

    Using icons for qtile groups instead of text

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -1,29 +1,3 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 from libqtile import bar, layout, qtile, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
@@ -112,8 +86,12 @@ for vt in range(1, 8):
 #        #     desc="move focused window to group {}".format(i.name)),
 #    ])
 
+# groups = [Group(i) for i in [
+#     "www", "term", "code", "dev", "docs", "chat", "mgt", "media", "misc",
+# ]]
+
 groups = [Group(i) for i in [
-    "www", "term", "code", "dev", "docs", "chat", "mgt", "media", "misc",
+    " 󰈹 ", "   ", "   ", "   ", " 󰦨 ", "   ", "  ", " 󰝚 ", " 󱄅 ",
 ]]
 
 for i, group in enumerate(groups):
@@ -140,8 +118,8 @@ layouts = [
 ]
 
 widget_defaults = dict(
-    font="UbuntuMono Nerd Font Bold",
-    fontsize=14,
+    font="Ubuntu Nerd Font Bold",
+    fontsize=15,
     padding=2,
 )
 extension_defaults = widget_defaults.copy()
@@ -190,6 +168,7 @@ screens = [
                     # background="#2E3440",
                     highlight_method='block',
                     padding=5,
+                    fontsize=16
                 ),
                 widget.Prompt(),
                 widget.WindowName(
