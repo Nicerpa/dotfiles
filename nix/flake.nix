70ef599b7174f90e6dbbfc05927ab0342dd7cfd0
Nicolás Cerpa <nicolas.cerpa@sansano.usm.cl>
2024-12-23 20:38:15 -0300

    Modularizing nix config files and adding hosts

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,19 +4,36 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./configuration.nix
-        # inputs.home-manager.nixosModules.default
-      ];
+    nixosConfigurations = {
+      default = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/default/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+      scanner = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/scanner/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+      operator = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/operator/configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
     };
+    
   };
 }
