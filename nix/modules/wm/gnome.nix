ed425eaa428d7b88826e12e6724ca6ee593afda4
Nicolás Cerpa <nicerpa@gmail.com>
2025-09-24 16:48:26 -0300

    Fix gnome module

--- nix/modules/wm/gnome.nix
+++ nix/modules/wm/gnome.nix
@@ -7,11 +7,9 @@ with lib;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  services.gnome = {
-    core-apps.enable = false;
-    core-developer-tools.enable = false;
-    games.enable = false;
-  };
+  services.gnome.core-apps.enable = false;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
 
   environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs ];
 }
